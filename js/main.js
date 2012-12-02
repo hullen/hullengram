c78f0a53da3cf88c6e48bc38182096f77e18b7e7
Hullen Gonzales <hullenn@gmail.com>
2012-12-02 21:20:33 -0200

    sending files to git

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function() { 
+	var max_img = 6,
+	$insta_list = $('#insta_media');
+	$.ajax({
+	  type: "GET",
+     dataType: "jsonp",
+     cache: false,
+	  url: "https://api.instagram.com//v1/users/1910204/media/recent?count=" + max_img + "&amp;access_token=1910204.1fb234f.55bf74fc1df246a584191dcf5909eae8",
+	  success: function(obj){
+			$.each(obj.data, function (k,insta) {
+				var caption = insta.caption;
+				if(caption == null) {
+					$insta_list.append('<li class="span2"><a href="' + insta.link + '" class="thumbnail"><img src="' + insta.images['thumbnail'].url + '" alt=""></a></li>');
+				} else {
+					$insta_list.append('<li class="span2"><a href="' + insta.link + '" class="thumbnail"><img src="' + insta.images['thumbnail'].url + '" alt="' + caption.text + '"></a><small>' + caption.text + '</small></li>');
+				}
+			});
+	  }
+	});
+
+});
